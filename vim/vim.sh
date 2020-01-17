1181f0753bc4bfa51f91320b432818593fddbac3
Tshilidzi Tshivhula <tshilidzi@vontae.co.za>
2020-01-17 17:05:14 +0200

    copy vimrc file

--- vim/vim.sh
+++ vim/vim.sh
@@ -4,5 +4,6 @@ if [ -d "$FILE" ]; then
     vim +PluginUpdate +qall
 else 
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    cp vimrc ~/.vimrc
     vim +PluginInstall +qall
 fi
